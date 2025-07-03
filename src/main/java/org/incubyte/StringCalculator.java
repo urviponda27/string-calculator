f5c403373a35d72beae95cdb87f764cbdfecb043
urviponda27 <urviponda27@gmail.com>
2025-07-04 00:20:55 +0530

    test: add failing test for empty string input

--- src/main/java/org/incubyte/StringCalculator.java
+++ src/main/java/org/incubyte/StringCalculator.java
@@ -1,5 +1,9 @@
 package org.incubyte;
 
 public class StringCalculator {
-    
+    public static final String EMPTY_STRING = "";
+
+    public static String calculateSum(String input) {
+        return null; // Not implemented yet
+    }
 }
--- src/test/java/org/incubyte/StringCalculatorTest.java
+++ src/test/java/org/incubyte/StringCalculatorTest.java
@@ -1,7 +1,11 @@
 package org.incubyte;
-
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
-
+    @Test
+    public void emptyStringShouldReturnZero() {
+        assertThat(StringCalculator.calculateSum("")).isEqualTo("0");
+    } 
    
 }
