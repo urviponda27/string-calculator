fb458dfc781c1d060690460a361cc96ffb7e0894
urviponda27 <urviponda27@gmail.com>
2025-07-04 09:53:31 +0530

    feat: add support for summing comma-separated numbers

--- src/main/java/org/incubyte/StringCalculator.java
+++ src/main/java/org/incubyte/StringCalculator.java
@@ -1,12 +1,21 @@
 package org.incubyte;
 
 public class StringCalculator {
+    public static final String COMMA = ",";
     public static final String EMPTY_STRING = "";
 
     public static String calculateSum(String input) {
        if (input.equals(EMPTY_STRING)) {
         return "0";
      }
-        return input;
+        return addAllNumbers(input.split(COMMA));
+    }
+     // Add all numbers in the given string array and return the sum as a string.
+    private static String addAllNumbers(String[] numbers) {
+        int sum = 0;
+        for (String numberString : numbers) {
+            sum = sum + Integer.valueOf(numberString);
+        }
+        return String.valueOf(sum);
     }
 }
