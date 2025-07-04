94cab90e6cf9a9dbf98ba25565df222c4693f2fb
urviponda27 <urviponda27@gmail.com>
2025-07-04 09:11:51 +0530

    test: add failing test for sum of numbers separated by comma

--- src/test/java/org/incubyte/StringCalculatorTest.java
+++ src/test/java/org/incubyte/StringCalculatorTest.java
@@ -4,12 +4,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
     @Test
-    public void emptyStringShouldReturnZero() {
+    public void emptyStringShouldReturnZero()
+    {
         assertThat(StringCalculator.calculateSum("")).isEqualTo("0");
     } 
     @Test
-    public void singleStringShouldReturnItselfAsSum() {
+    public void singleStringShouldReturnItselfAsSum()
+     {
     assertThat(StringCalculator.calculateSum("5")).isEqualTo("5");
-}
+     }
+     @Test
+    public void sumOfNumbersSeperatedByCommaShouldReturned()
+    {
+        assertThat(StringCalculator.calculateSum("1,2,3")).isEqualTo("6");
+
+    }
 
 }
